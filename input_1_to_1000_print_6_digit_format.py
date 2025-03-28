d34fb989d86ede459c979f27cedd69a554effedc
CedDesu <cedriqueorbista@gmail.com>
2025-03-28 15:15:16 +0800

    Added final code and tested for working condition

--- input_1_to_1000_print_6_digit_format.py
+++ input_1_to_1000_print_6_digit_format.py
@@ -4,3 +4,6 @@ Input 4 digits and ask to limit it between 1 and 1000 only
 print in a 6 digit format using :06d inside the print function
 """
 
+input_num = int(input("Input num 1-1000: "))
+
+print(f"{input_num:06d}")
