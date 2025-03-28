dcba793d62eb03fc92f54f26fa677c7f4d09af14
CedDesu <cedriqueorbista@gmail.com>
2025-03-28 15:46:54 +0800

    added pseudocode and copied format of word counter program

---
+++ input_word_or_sentence_count_character.py
@@ -0,0 +1,11 @@
+"""
+
+use same format as word counter and remove .split() from .len()
+
+user_sentence = input("Please type a sentence: ")
+
+sentence = len(user_sentence.split())
+
+print(sentence)
+
+"""
