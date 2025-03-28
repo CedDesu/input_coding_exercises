fd5bddcd48f404f85aef09cd43eba221d8ecd39b
CedDesu <cedriqueorbista@gmail.com>
2025-03-28 15:40:34 +0800

    added final code and tested word counter

--- input_sentence_count_words.py
+++ input_sentence_count_words.py
@@ -6,3 +6,9 @@ variable = len.split   -  use len to count and use split to count the whole word
 print user input
 
 """
+
+user_sentence = input("Please type a sentence: ")
+
+sentence = len(user_sentence.split())
+
+print(sentence)
